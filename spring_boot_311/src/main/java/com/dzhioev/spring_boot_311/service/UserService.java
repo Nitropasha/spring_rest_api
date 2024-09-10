690229688e165f83949aef19a9dabbb0b1cb1ef2
nitropasha <djioeff@gmail.com>
2024-09-10 17:47:54 +0500

    2024 Boot new

--- spring_boot_311/src/main/java/com/dzhioev/spring_boot_311/service/UserService.java
+++ spring_boot_311/src/main/java/com/dzhioev/spring_boot_311/service/UserService.java
@@ -6,8 +6,8 @@ import com.dzhioev.spring_boot_311.entity.User;
 import java.util.List;
 
 public interface UserService {
-     public List<User> listUsers();
-     public void saveUser(User user);
-     public User getUser(Long id);
-     public void deleteUser(Long id);
+      List<User> listUsers();
+      void saveUser(User user);
+      User getUser(Long id);
+      void deleteUser(Long id);
 }
