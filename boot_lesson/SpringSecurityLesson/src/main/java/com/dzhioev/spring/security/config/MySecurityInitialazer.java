27e6234ab823ccf832f07304213d8c02f54da47b
Nitropasha <djioeff@gmail.com>
2024-09-14 18:33:39 +0500

    Delete boot_lesson directory

--- boot_lesson/SpringSecurityLesson/src/main/java/com/dzhioev/spring/security/config/MySecurityInitialazer.java
+++
@@ -1,6 +0,0 @@
-package com.dzhioev.spring.security.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class MySecurityInitialazer extends AbstractSecurityWebApplicationInitializer {
-}
