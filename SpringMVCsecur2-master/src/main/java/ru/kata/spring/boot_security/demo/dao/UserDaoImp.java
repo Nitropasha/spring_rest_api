551fbab343c759c27a668ff4ddd7145364af9f8d
nitropasha <djioeff@gmail.com>
2024-09-17 08:38:45 +0500

    NEw

--- SpringMVCsecur2-master/src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImp.java
+++ SpringMVCsecur2-master/src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImp.java
@@ -1,12 +1,8 @@
 package ru.kata.spring.boot_security.demo.dao;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
-
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
